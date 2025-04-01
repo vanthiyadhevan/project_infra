e260139bc2b372dc4c929a4c35c8abdd0361fe88
vanthiyadhevan <samvickey96@gmail.com>
2025-04-01 11:47:07 +0530

    updated provider and hardcoded env path

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
-provider "aws" {
-  region  = var.region
-  version = "5.90.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.90.0"
+    }
+  }
 }
 # terraform {
 #   backend "s3" {
